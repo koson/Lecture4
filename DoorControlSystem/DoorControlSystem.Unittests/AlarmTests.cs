c985cfc6fa1ca25d48097c9aaa8bdf0552196089
phil2988 <phillipblicher@hotmail.com>
2021-09-21 10:14:34 +0100

    Update AlarmTests.cs

--- DoorControlSystem/DoorControlSystem.Unittests/AlarmTests.cs
+++ DoorControlSystem/DoorControlSystem.Unittests/AlarmTests.cs
@@ -6,19 +6,19 @@ namespace DoorControlSystem.Unittests
     public class AlarmTests
     {
         Alarm alarm;
-        FakeAlarm fakeAlarm;
         [SetUp]
         public void Setup()
         {
             
         }
 
-        [TestCase]
+        [Test]
         public void Alarm_AlarmIsSounding_ReturnTrue()
         {
             alarm = new Alarm();
             alarm.RaiseAlarm();
             Assert.IsTrue(alarm.alarmSounding);
         }
+        
     }
 }
