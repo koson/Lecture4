7c8b61a7b841d1d28c0fe2dbea1bdb9c2d2114d1
Inferian <karolperkowski.main@gmail.com>
2021-09-21 10:08:57 +0200

    added alarm, Ialarm and enum

---
+++ DoorControl/DoorControl/Alarm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControl
+{
+    class Alarm : IAlarm
+    {
+        public bool isAlarmed { get; private set; } = false;
+        public void RaiseAlarm()
+        {
+            
+        }
+    }
+}
---
+++ DoorControl/DoorControl/DoorControlState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControl
+{
+    public enum DoorControlState
+    {
+        DoorClosed,
+        DoorClosing, 
+        DoorOpen,
+        DoorOpening,
+        DoorBreached
+    }
+}
